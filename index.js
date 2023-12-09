5236dd4603ee126b57e029c92e297fc321994b99
Reinaldo Loero <rloero89@gmail.com>
2023-12-09 15:13:22 -0800

    Complete test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const thanksMsgs = [];
+    for(let i=0; i <= names.length - 1; i++){
+        thanksMsgs.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thanksMsgs;
+}
+
+function countDown(number){
+    for(let i = number; i>=0; i--){
+        console.log(i);
+    }
+}
